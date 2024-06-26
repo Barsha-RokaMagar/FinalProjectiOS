995a40c5aeab99fe12843a874b00400514315ea5
Barsha-RokaMagar <barsharoka2626@gmail.com>
2024-06-26 09:04:28 -0400

    Initial Commit

---
+++ FinalPrjoectiOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinalPrjoectiOS
+//
+//  Created by ART on 2024-06-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinalPrjoectiOS/FinalPrjoectiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalPrjoectiOSApp.swift
+//  FinalPrjoectiOS
+//
+//  Created by ART on 2024-06-26.
+//
+
+import SwiftUI
+
+@main
+struct FinalPrjoectiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
