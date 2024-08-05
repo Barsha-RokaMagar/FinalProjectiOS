e894293b9cbdb7097d5584ae69e44e26f619ee93
Barsha-RokaMagar <barsharoka2626@gmail.com>
2024-08-05 18:12:29 -0400

    making appointment

---
+++ FinalPrjoectiOS/Preview Content/AppointmentDetailsView.swift
@@ -0,0 +1,74 @@
+import SwiftUI
+
+struct AppointmentDetailsView: View {
+    var appointment: Appointment?
+
+    @Environment(\.presentationMode) var presentationMode
+
+    var body: some View {
+        VStack {
+            Text("Appointment Confirmation")
+                .font(.largeTitle)
+                .padding(.top)
+
+            if let appointment = appointment {
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Patient: \(appointment.patientName)")
+                        .font(.title2)
+                    Text("Doctor: \(appointment.cardiologist)")
+                        .font(.title2)
+                    Text("Date: \(appointment.date)")
+                        .font(.title2)
+                    Text("Time: \(appointment.time)")
+                        .font(.title2)
+                }
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 1))
+                .padding(.horizontal)
+            }
+
+            Text("Your appointment has been successfully booked. Thank you.")
+                .font(.title2)
+                .padding(.top)
+
+            Spacer()
+
+            Button(action: {
+                self.presentationMode.wrappedValue.dismiss()
+            }) {
+                Text("Go Back")
+                    .bold()
+                    .frame(maxWidth: .infinity, minHeight: 50)
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    .padding(.horizontal, 16)
+            }
+            .padding(.bottom, 16)
+        }
+        .padding()
+        .navigationTitle("Appointment Details")
+    }
+}
+
+struct Appointment: Identifiable {
+    var id: String
+    var date: String
+    var time: String
+    var cardiologist: String
+    var patientId: String
+    var patientName: String
+}
+
+struct AppointmentDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppointmentDetailsView(appointment: Appointment(
+            id: "1",
+            date: "Aug 5, 2024",
+            time: "10:00 AM",
+            cardiologist: "Dr. John Doe",
+            patientId: "abc123",
+            patientName: "Jane Doe"
+        ))
+    }
+}
--- FinalPrjoectiOS/Preview Content/CardiologistView.swift
+++ FinalPrjoectiOS/Preview Content/CardiologistView.swift
@@ -7,97 +7,102 @@ struct CardiologistView: View {
     @State private var selectedCardiologist: String?
     @State private var showAlert = false
     @State private var alertMessage = ""
-    
+    @State private var appointment: Appointment?
     @State private var cardiologists = [String]()
-    
+    @State private var showAppointmentDetails = false
+
     @Environment(\.presentationMode) var presentationMode
-    
+    @Environment(\.dismiss) var dismiss
+
     var body: some View {
-        VStack {
-            Image(systemName: "heart.fill")
-                .resizable()
-                .frame(width: 150, height: 100)
-                .padding(.top, 16)
-                .foregroundColor(.red)
-            
-            Text("Doctor Selection and Appointment")
-                .font(.headline)
-                .padding(.top, 8)
-            
-            VStack(alignment: .leading) {
-                Text("Doctor List - Cardiologist")
-                    .font(.subheadline)
-                    .bold()
-                    .padding(.bottom, 8)
+        NavigationStack {
+            VStack {
+                Image(systemName: "heart.fill")
+                    .resizable()
+                    .frame(width: 150, height: 100)
+                    .padding(.top, 16)
+                    .foregroundColor(.red)
                 
-                ScrollView {
-                    VStack(alignment: .leading) {
-                        ForEach(cardiologists, id: \.self) { cardiologist in
-                            RadioButton(text: cardiologist, isSelected: self.selectedCardiologist == cardiologist) {
-                                self.selectedCardiologist = cardiologist
+                Text("Doctor Selection and Appointment")
+                    .font(.headline)
+                    .padding(.top, 8)
+                
+                VStack(alignment: .leading) {
+                    Text("Doctor List - Cardiologist")
+                        .font(.subheadline)
+                        .bold()
+                        .padding(.bottom, 8)
+                    
+                    ScrollView {
+                        VStack(alignment: .leading) {
+                            ForEach(cardiologists, id: \.self) { cardiologist in
+                                RadioButton(text: cardiologist, isSelected: self.selectedCardiologist == cardiologist) {
+                                    self.selectedCardiologist = cardiologist
+                                }
                             }
                         }
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 10).stroke(Color.gray))
                     }
-                    .padding()
-                    .background(RoundedRectangle(cornerRadius: 10).stroke(Color.gray))
                 }
-            }
-            .padding()
-            
-            VStack {
-                HStack {
-                    DatePicker("Select Date", selection: $selectedDate, displayedComponents: .date)
-                        .padding()
-                        .background(RoundedRectangle(cornerRadius: 10).stroke(Color.gray))
-                }
-                .padding(.top, 8)
+                .padding()
                 
-                HStack {
-                    DatePicker("Select Time", selection: $selectedTime, displayedComponents: .hourAndMinute)
-                        .padding()
-                        .background(RoundedRectangle(cornerRadius: 10).stroke(Color.gray))
-                }
-                .padding(.top, 8)
-            }
-            .padding()
-            
-            Button(action: bookAppointment) {
-                Text("Book an Appointment")
-                    .bold()
-                    .frame(maxWidth: .infinity, minHeight: 50)
-                    .foregroundColor(.white)
-                    .background(Color.blue)
-                    .cornerRadius(10)
+                VStack {
+                    HStack {
+                        DatePicker("Select Date", selection: $selectedDate, displayedComponents: .date)
+                            .padding()
+                            .background(RoundedRectangle(cornerRadius: 10).stroke(Color.gray))
+                    }
                     .padding(.top, 8)
+                    
+                    HStack {
+                        DatePicker("Select Time", selection: $selectedTime, displayedComponents: .hourAndMinute)
+                            .padding()
+                            .background(RoundedRectangle(cornerRadius: 10).stroke(Color.gray))
+                    }
+                    .padding(.top, 8)
+                }
+                .padding()
+                
+                Button(action: bookAppointment) {
+                    Text("Book an Appointment")
+                        .bold()
+                        .frame(maxWidth: .infinity, minHeight: 50)
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        .padding(.top, 8)
+                }
+                .alert(isPresented: $showAlert) {
+                    Alert(title: Text("Message"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+                }
+                .padding(.horizontal, 16)
+                .background(
+                    NavigationLink(
+                        destination: AppointmentDetailsView(appointment: appointment),
+                        isActive: $showAppointmentDetails,
+                        label: { EmptyView() }
+                    )
+                )
+                
+                Spacer()
             }
-            .alert(isPresented: $showAlert) {
-                Alert(title: Text("Message"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
-            }
-            .padding(.horizontal, 16)
-            
-            Spacer()
+            .onAppear(perform: loadCardiologists)
+            .padding()
         }
-        .onAppear(perform: loadCardiologists)
-        .padding()
     }
     
     private func loadCardiologists() {
-        let usersRef = Database.database().reference().child("users")
-        
-        usersRef.observe(.value) { snapshot in
+        let cardiologistRef = Database.database().reference().child("users").queryOrdered(byChild: "specialty").queryEqual(toValue: "Cardiologist")
+        cardiologistRef.observe(.value) { snapshot in
             var newCardiologists = [String]()
-            
             for child in snapshot.children {
                 if let snapshot = child as? DataSnapshot,
-                   let dict = snapshot.value as? [String: Any] {
-                    
-                    if let specialty = dict["specialty"] as? String, specialty == "Cardiologist",
-                       let name = dict["name"] as? String {
-                        newCardiologists.append(name)
-                    }
+                   let dict = snapshot.value as? [String: Any],
+                   let name = dict["name"] as? String {
+                    newCardiologists.append(name)
                 }
             }
-            
             self.cardiologists = newCardiologists
         }
     }
@@ -111,19 +116,24 @@ struct CardiologistView: View {
         
         let dateFormatter = DateFormatter()
         dateFormatter.dateStyle = .medium
-        dateFormatter.timeStyle = .short
+        
+        let timeFormatter = DateFormatter()
+        timeFormatter.timeStyle = .short
         
         let date = dateFormatter.string(from: selectedDate)
-        let time = dateFormatter.string(from: selectedTime)
+        let time = timeFormatter.string(from: selectedTime)
         
         let patientId = Auth.auth().currentUser?.uid ?? ""
+        let patientName = Auth.auth().currentUser?.displayName ?? "Unknown"
+        
         let appointmentRef = Database.database().reference().child("appointments").childByAutoId()
         
         let appointmentData: [String: Any] = [
             "date": date,
             "time": time,
             "cardiologist": selectedCardiologist,
-            "patientId": patientId
+            "patientId": patientId,
+            "patientName": patientName
         ]
         
         appointmentRef.setValue(appointmentData) { error, _ in
@@ -131,8 +141,15 @@ struct CardiologistView: View {
                 self.alertMessage = "Failed to book appointment: \(error.localizedDescription)"
                 self.showAlert = true
             } else {
-                self.alertMessage = "Appointment booked"
-                self.showAlert = true
+                self.appointment = Appointment(
+                    id: appointmentRef.key ?? "",
+                    date: date,
+                    time: time,
+                    cardiologist: selectedCardiologist,
+                    patientId: patientId,
+                    patientName: patientName
+                )
+                self.showAppointmentDetails = true
             }
         }
     }
@@ -165,3 +182,4 @@ struct CardiologistView_Previews: PreviewProvider {
         CardiologistView()
     }
 }
+
