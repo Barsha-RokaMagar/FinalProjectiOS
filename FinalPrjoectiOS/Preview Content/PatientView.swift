552beaad2feb0397491eb174fde216fc97f3b40a
Barsha-RokaMagar <barsharoka2626@gmail.com>
2024-08-05 16:39:31 -0400

    linking with different parts of doctors speciality

--- FinalPrjoectiOS/Preview Content/PatientView.swift
+++ FinalPrjoectiOS/Preview Content/PatientView.swift
@@ -29,7 +29,7 @@ struct PatientView: View {
                         }
                         NavigationLink(destination: DentistView()) {
                             SpecialistButton(icon: "bandage.fill", title: "Dentist")
-                            }
+                        }
                         NavigationLink(destination: PsychologistView()) {
                             SpecialistButton(icon: "person.2.fill", title: "Psychologist")
                         }
@@ -37,61 +37,71 @@ struct PatientView: View {
                     HStack {
                         NavigationLink(destination: DermatologistView()) {
                             SpecialistButton(icon: "pills.fill", title: "Dermatologist")
-                                               }
-                        SpecialistButton(icon: "person.fill", title: "Pediatrician")
-                        SpecialistButton(icon: "eye.fill", title: "Ophthalmologist")
+                        }
+                        NavigationLink(destination: PediatricianView()) {
+                            SpecialistButton(icon: "person.fill", title: "Pediatrician")
+                        }
+                        NavigationLink(destination: OpthalmologistView()) {
+                            SpecialistButton(icon: "eye.fill", title: "Ophthalmologist")
+                        }
                     }
                     HStack {
-                        SpecialistButton(icon: "brain.head.profile", title: "Neurologist")
-                        SpecialistButton(icon: "staroflife.fill", title: "Gynecologist")
+                        NavigationLink(destination: NeurologistView()) {
+                            SpecialistButton(icon: "brain.head.profile", title: "Neurologist")
+                        }
+                        NavigationLink(destination: GynecologistView()) {
+                            SpecialistButton(icon: "staroflife.fill", title: "Gynecologist")
+                        }
+                      }
                     }
+                    .padding()
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        
+                    }) {
+                        Text("Logout")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    }
+                    .padding(.bottom)
                 }
-                .padding()
                 
-                Spacer()
-                
-                Button(action: {
-                   
-                }) {
-                    Text("Logout")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.blue)
-                        .cornerRadius(10)
-                }
-                .padding(.bottom)
             }
-            
         }
     }
-}
-
-struct SpecialistButton: View {
-    var icon: String
-    var title: String
     
-    var body: some View {
-        VStack {
-            Image(systemName: icon)
-                .resizable()
-                .scaledToFit()
-                .frame(width: 50, height: 50)
-            Text(title)
-                .font(.subheadline)
-                .multilineTextAlignment(.center)
+    struct SpecialistButton: View {
+        var icon: String
+        var title: String
+        
+        var body: some View {
+            VStack {
+                Image(systemName: icon)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 50, height: 50)
+                Text(title)
+                    .font(.subheadline)
+                    .multilineTextAlignment(.center)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, minHeight: 100)
+            .background(Color.gray.opacity(0.1))
+            .cornerRadius(10)
         }
-        .padding()
-        .frame(maxWidth: .infinity, minHeight: 100)
-        .background(Color.gray.opacity(0.1))
-        .cornerRadius(10)
     }
-}
-
-
-
-struct PatientView_Previews: PreviewProvider {
-    static var previews: some View {
-        PatientView()
+    
+    
+    
+    struct PatientView_Previews: PreviewProvider {
+        static var previews: some View {
+            PatientView()
+        }
     }
-}
+
+
