73528579065a6b76cec3900861bc0ccb8677e99c
Barsha-RokaMagar <barsharoka2626@gmail.com>
2024-08-05 11:21:39 -0400

    creating new page

---
+++ FinalPrjoectiOS/Preview Content/CardiologistView.swift
@@ -0,0 +1,18 @@
+//
+//  CardiologistView.swift
+//  FinalPrjoectiOS
+//
+//  Created by ART on 2024-08-05.
+//
+
+import SwiftUI
+
+struct CardiologistView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardiologistView()
+}
